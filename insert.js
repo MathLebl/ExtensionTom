137778eba195b5b4cee79c889e23f45e895ab76e
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-02 16:29:34 +0200

    target Url set

--- insert.js
+++ insert.js
@@ -1,6 +1,6 @@
 const origin = document.querySelector('html').innerHTML;
 console.log(origin)
-const contentRight = '<div style="height:800px; width:30%; margin-top:100px; margin: 10px; border:1px solid blue; border-radius:20px; position:fixed; right: 0px"><iframe src="https://www.sofoot.com/" width=100% height=100%></iframe></div>'
+const contentRight = '<div style="height:800px; width:30%; margin-top:100px; margin: 10px; border:1px solid blue; border-radius:20px; position:fixed; right: 0px"><iframe src="https://readymag.com/u38532320/1883919/" width=100% height=100%></iframe></div>'
 
 
 
